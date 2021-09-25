1221e5ed3c4125df0b7f6929e5a4f12f76ec8b9b
hdolci <huydo670@gmail.com>
2021-09-24 19:27:19 -0700

    adding initial challenge files

---
+++ MechaCarChallenge.R
@@ -0,0 +1,25 @@
+#Part  1
+library(dplyr)
+
+MechaCar <- read.csv(file='MechaCar_mpg.csv',check.names=F,stringsAsFactors = F)
+
+lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AWD,data= MechaCar)
+
+summary(lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AWD,data= MechaCar))
+
+
+#Part 2
+Suspension <- read.csv(file='Suspension_coil.csv',check.names=F,stringsAsFactors = F)
+
+total_summary <- Suspension %>% summarize(Mean=mean(PSI),Median=(PSI),Variance=var(PSI),SD=sd(PSI)) 
+
+lot_summary <- Suspension %>% group_by(Manufacturing_Lot) %>% summarize(Mean=mean(PSI),Median=(PSI),Variance=var(PSI),SD=sd(PSI)) 
+
+#Part 3
+t.test(Suspension$PSI,mu = 1500)
+
+t.test(subset(Suspension,Manufacturing_Lot=="Lot1")$PSI,mu = 1500)
+
+t.test(subset(Suspension,Manufacturing_Lot=="Lot2")$PSI,mu = 1500)
+
+t.test(subset(Suspension,Manufacturing_Lot=="Lot3")$PSI,mu = 1500)
